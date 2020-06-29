43ca27d8a764841fac175afc262c527f0ae7539d
Ron <li.rongcheng@gmail.com>
2020-06-29 13:23:05 +1000

    add geosense+ cli build

--- release-gsv.bat
+++ release-gsv.bat
@@ -6,11 +6,21 @@ SET SOLUTION_DIR=E:\rp\outdoor-asset-tracking\github\GeoSensePluse-Server
 SET GSV_VERSION=%1
 docker build --no-cache -t redwoodteq/geosenseplusserver:latest -t redwoodteq/geosenseplusserver:%GSV_VERSION% -f %SOLUTION_DIR%\GeoSensePlus.Server\Dockerfile %SOLUTION_DIR%
 
-:: pack to nuget package
-:: =====================
+:: pack nuget package
+:: ==================
 
-SET PROJECT_NAME=GeoSensePlus.Server
 SET PACK_OUTPUT_DIR=E:\rp\local-nuget-packages
 
+:: GeoSensePlus.Server
+:: -------------------
+
+SET PROJECT_NAME=GeoSensePlus.Server
+dotnet pack %SOLUTION_DIR%\%PROJECT_NAME%\%PROJECT_NAME%.csproj -c Release -o %PACK_OUTPUT_DIR%
+dotnet tool update -g %PROJECT_NAME% --add-source %PACK_OUTPUT_DIR%
+
+:: GeoSensePlus.Cli
+:: ----------------
+
+SET PROJECT_NAME=GeoSensePlus.Cli
 dotnet pack %SOLUTION_DIR%\%PROJECT_NAME%\%PROJECT_NAME%.csproj -c Release -o %PACK_OUTPUT_DIR%
 dotnet tool update -g %PROJECT_NAME% --add-source %PACK_OUTPUT_DIR%
